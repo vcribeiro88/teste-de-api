df19bccf86354593c1f4ae054c6d32138c81c703
Vinícius de Oliveira Ribeiro <vcribeiro88@gmail.com>
2020-11-27 11:43:34 -0400

    Initial Commit

---
+++ Using an API created by myself/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Using an API created by myself
+//
+//  Created by Vinícius de Oliveira on 27/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
